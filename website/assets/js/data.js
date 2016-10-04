fb8b29e123bb1a86b838ec76abc1cb4624d05cb3
newswim <ofenixculpa@gmail.com>
2016-10-04 14:00:01 -0500

    Add video backgrounds, more content

---
+++ website/assets/js/data.js
@@ -0,0 +1,20 @@
+// ===========
+// TODO
+// ===========
+
+/*
+
+  - Load in products from Shopify, preferably sorted by a collection
+  - Obfuscate Shopify API keys using the github environment
+  - Manipulate DOM directly by creating template items for each product
+          - jQuery is already included so this shouldn't be too painful
+
+
+
+More complicated tasks, such as tracking a user's cart and order information
+would require a more thorough integration with shopify, and probably using
+something like < https://github.com/Shopify/shopify_api >
+along with converting our website over to jekyll so that we can use the gem
+for deployment among other things.
+
+                                  /*
